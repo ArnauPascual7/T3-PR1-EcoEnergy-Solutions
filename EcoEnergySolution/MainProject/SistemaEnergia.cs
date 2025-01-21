958f68e2e500674874005296f2d6c2c5eaee9c52
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-01-21 10:46:05 +0100

    Abstract Class SistemaEnergia Created - Not Finished #1

---
+++ EcoEnergySolution/MainProject/SistemaEnergia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MainProject
+{
+    public abstract class SistemaEnergia : ICalculEnergia
+    {
+        public abstract void ConfigurarParametres();
+        public abstract float CalcularEnergia();
+        public void MostrarInforme(string configParName, float configPar)
+        {
+            Console.WriteLine($"| {"Tipus Sistema",-20} | {configParName,-20} | {"Energia Generada",-20} |");
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine($"| {GetType().Name,-20} | {configPar,-20:F2} | {CalcularEnergia(),-20:F2} |");
+        }
+    }
+}
